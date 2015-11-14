5055e5fd527dce3ab0ca001b12eb8d8b4d087827
Markus Rosjat <markus.rosjat@gmail.com>
2015-11-14 17:24:49 +0100

    adding method to handle commands from context menu
    also put in some debug logging messages

--- mtx_gui/Control/application.py
+++ mtx_gui/Control/application.py
@@ -54,13 +54,23 @@ class Application(Observable):
         action = callback_dict['action']
         if type(sender) == MediumChangerObserver and action == 'init':
             self._init_medium_changer_button(sender)
-        if type(sender) == StorageSlotObserver and action == 'contextmenu':
-            self._init_storage_slot_contextmenu(sender, event)
-        if type(sender) == DataSlotObserver and action == 'contextmenu':
-            self._init_data_slot_contextmenu(sender, event)
+        if type(sender) == StorageSlotObserver:
+            modul_logger.debug('storageslot')
+            if action == 'contextmenu':
+                self._init_storage_slot_contextmenu(sender, event)
+            if action == 'unload':
+                self._init_mc_command(sender, event, 'unload')
+        if type(sender) == DataSlotObserver:
+            modul_logger.debug('dataslot')
+            if action == 'contextmenu':
+                self._init_data_slot_contextmenu(sender, event)
+            if action == 'load':
+                modul_logger.debug('sender: %s event: %s' % (sender, event))
+                self._init_mc_command(sender, event, 'load')
 
     def _init_medium_changer_button(self, sender):
         # issue a modesense + elementstatus
+        sender.model.get_medium_transport_elements()
         sender.model.get_data_slots()
         sender.model.get_storage_slots()
         # now take the collected data and wrap it in nice Slot objects
@@ -76,19 +86,27 @@ class Application(Observable):
     def _init_storage_slot_contextmenu(self, sender, event):
         mc = [k for k, v in self._ss.items() if sender in v][0]
         with SlotMenu(sender.view,
+                      sender,
                       self._ds[mc],
                       u'load to %s',
                       tearoff=0) as popup:
+            modul_logger.debug('text: %s' % sender.view.text)
             if sender.view.text != 'empty':
-                modul_logger.debug('show contextmenu')
+                modul_logger.debug('show ss contextmenu')
                 popup.tk_popup(event.x_root, event.y_root, 0)
 
     def _init_data_slot_contextmenu(self, sender, event):
         mc = [k for k, v in self._ds.items() if sender in v][0]
         with SlotMenu(sender.view,
+                      sender,
                       self._ss[mc],
                       u'unload to %s',
                       tearoff=0) as popup:
+            modul_logger.debug('text: %s' % sender.view.text)
             if sender.view.text != 'empty':
-                modul_logger.debug('show contextmenu')
+                modul_logger.debug('show ds contextmenu')
                 popup.tk_popup(event.x_root, event.y_root, 0)
+
+    def _init_mc_command(self, sender, event, cmd):
+        mc = [k for k, v in self._ds.items() if sender in v][0]
+        mc.do_command(sender, event, cmd)
